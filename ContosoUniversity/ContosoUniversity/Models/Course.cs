f8c24c13d8e79917c889ddd232632ffef5cab1d1
Anastasia Jevdokimova <a.jevdokimova98@gmail.com>
2021-09-27 11:44:55 +0300

    CreatingADatabaseContext

--- ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        /*Свойство Enrollments является свойством навигации. Сущность Course может быть связана с любым числом сущностей Enrollment.
+         * Атрибут DatabaseGenerated описан в следующем учебнике. Этот атрибут позволяет ввести ПК для курса, а не использовать базу данных, чтобы создать его.
+         */
     }
 }
--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -1,11 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ContosoUniversity.Models
+﻿namespace ContosoUniversity.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
     public class Enrollment
     {
+        public int EnrollmentID { get; set; }
+        
+        /* Свойство EnrollmentID — это ПК. Эта сущность использует шаблон classnameID вместо самого по себе ID.
+         * Сущность Student использовала шаблон ID. 
+         */
+        public int CourseID { get; set; }
+        
+         /* Свойство CourseID представляет собой ВК. Ему соответствует свойство навигации Course.
+         * Сущность Enrollment связана с одной сущностью Course.
+         */
+        public int StudentID { get; set; }
+        
+         /* Свойство StudentID представляет собой внешний ключ (ВК). Ему соответствует свойство навигации Student. 
+         * Сущность Enrollment связана с одной сущностью Student, поэтому свойство содержит только отдельную сущность 
+         * Student. Она отличается от свойства навигации Student.Enrollments, которое содержит несколько сущностей 
+         * Enrollment.
+         */
+        public Grade? Grade { get; set; }
+
+        /*Свойство Grade имеет тип enum. Знак ? после объявления типа Grade указывает, что свойство Grade допускает 
+         * значение NULL. Оценка со значением null отличается от нулевой оценки. При значении null оценка еще не известна 
+         * или не назначена.
+         */
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
     }
 }
