ca1a054500a20ef0bcf555fe0c44c87bde91508b
Anastasia Jevdokimova <a.jevdokimova98@gmail.com>
2021-10-11 13:42:47 +0300

    migrations

--- ContosoUniversity/ContosoUniversity/Migrations/20211011103047_InitialCreate.cs
+++ ContosoUniversity/ContosoUniversity/Migrations/20211011103047_InitialCreate.cs
@@ -1,29 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-public partial class InitialCreate : Migration
+namespace ContosoUniversity.Migrations
 {
-    protected override void Up(MigrationBuilder migrationBuilder)
+    public partial class InitialCreate : Migration
     {
-        migrationBuilder.CreateTable(
-            name: "Course",
-            columns: table => new
-            {
-                CourseID = table.Column<int>(nullable: false),
-                Credits = table.Column<int>(nullable: false),
-                Title = table.Column<string>(nullable: true)
-            },
-            constraints: table =>
-            {
-                table.PrimaryKey("PK_Course", x => x.CourseID);
-            });
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(nullable: false),
+                    Credits = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.CourseID);
+                });
 
-        // Additional code not shown
-    }
+            // Additional code not shown
+        }
 
-    protected override void Down(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.DropTable(
-            name: "Enrollment");
-        // Additional code not shown
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enrollment");
+            // Additional code not shown
+        }
     }
 }
