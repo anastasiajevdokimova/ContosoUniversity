be7bf5fe140bdd66b2a8c074437187133d82d4e7
Anastasia Jevdokimova <a.jevdokimova98@gmail.com>
2021-11-01 13:34:32 +0200

    Update Details and Create views

--- ContosoUniversity/ContosoUniversity/Controllers/DepartmentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/DepartmentsController.cs
@@ -156,7 +156,7 @@ namespace ContosoUniversity.Controllers
         // POST: Departments/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        /*[HttpPost]
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("DepartmentID,Name,Budget,StartDate,InstructorID,RowVersion")] Department department)
         {
@@ -190,7 +190,12 @@ namespace ContosoUniversity.Controllers
             return View(department);
            
         }
-        */
+
+        private bool DepartmentExists(int departmentID)
+        {
+            throw new NotImplementedException();
+        }
+
         // GET: Departments/Delete/5
         public async Task<IActionResult> Delete(int? id, bool? concurrencyError)
         {
